52e588b99b2d34cfb3d4f06e789ec13aa6b67b7c
Mingshuo Zhang <mszhang0220@gmail.com>
2017-01-04 23:21:31 -0800

    add header and footer skeleton

--- common/components/index.jsx
+++ common/components/index.jsx
@@ -1,20 +1,19 @@
-import React, { Component, PropTypes } from "react";
-import ES6Promise from "es6-promise";
+import React, { Component } from 'react';
+import ES6Promise from 'es6-promise';
 
-// import Header from "./header";
+import Header from './header';
+import Footer from './footer'
 
 export default class App extends Component {
-  static propTypes = {
-    children: PropTypes.object
-  };
 
   render() {
     // for ie 11 ...
     ES6Promise.polyfill();
     return (
-      <div id="app">
-        <h2>Header!</h2>
+      <div id='app'>
+        <Header />
         {this.props.children}
+        <Footer />
       </div>
     );
   }
