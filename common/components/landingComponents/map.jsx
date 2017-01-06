7f5a71eeeaa83ed9de26b548e8a45cfac7ac08f6
Mingshuo Zhang <mszhang0220@gmail.com>
2017-01-06 15:40:56 -0800

    map finished

--- common/components/landingComponents/map.jsx
+++ common/components/landingComponents/map.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
+const GoogleMapApiKey = 'AIzaSyAKUAgQyzFI9Ad095DJzBQQeTRLukQwQPs'
+
 export default class Map extends Component {
   // TODO: google map api is here
   render() {
     return (
-      <div>Map</div>
+      <iframe
+        width="100%"
+        height="350"
+        frameBorder="0"
+        style={{border: '0'}}
+        src= {"https://www.google.com/maps/embed/v1/place?q=7725%20Enterprise%20Dr%2C%20Newark%2C%20CA%2094560&key="+GoogleMapApiKey}
+        allowFullScreen>
+      </iframe>
     )
   }
 }
